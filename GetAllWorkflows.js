fe77d6d40df7da80fb1f94d91708357ba869c2c0
Nick Charvat <nickcharvat@gmail.com>
2021-05-05 13:11:09 -0700

    update getAllWorkflows

--- GetAllWorkflows.js
+++ GetAllWorkflows.js
@@ -5,7 +5,6 @@ exports.handler = (event, context, callback) => {
     const query = `
         SELECT * 
         FROM workflows 
-        WHERE user_id = '${event.queryStringParameters.user_id}'
         `;
 
     const client = new Client(dbConfig);
@@ -19,7 +18,8 @@ exports.handler = (event, context, callback) => {
     client.query(query, (err, res) => {
         if (err) {
             callback(err);
-        } else {
+        }
+        else {
             const response = {
                 statusCode: 200,
                 body: JSON.stringify(res.rows),
